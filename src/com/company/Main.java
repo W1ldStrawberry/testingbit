22b72719e47191e481628fd2b76871f714cda1e7
W1ldStrawberry <maxonchik372@yandex.ru>
2021-02-14 01:47:58 +0300

    lab3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
 //Объявление и запись рандомного массива
-        int n = 10;
+        int n = 50;
         int min = 1000;
         int max = -1;
         int avg = 0;
         int k = 0;
         int swap=0;
+        boolean flag = false;
         int[] array = new int[n];
         for (int i = 0; i< array.length;i++) {
-            array[i] = (int)(Math.random()*(200+1)-100);
+            array[i] = (int)(Math.random()*(200+1));
         }
 //поиск мин макс и среднего
         for (int i = 0; i< array.length;i++){
@@ -21,6 +22,7 @@ public class Main {
             max = Math.max(max,array[i]);
             avg += (int)(array[i] / array.length);
         }
+
         //сортировка пузырьком
         for (int i=0; i< array.length-1; i++){
             for (int j=0; j<array.length-i-1; j++){
@@ -30,7 +32,22 @@ public class Main {
              array[j+1]=swap; }
             }
             }
+        //Поиск простых чисел в массиве
+        System.out.printf("Prostie chisla v massive: ");
+        for (int i=0; i<array.length; i++){
+            if (array[i]>1){
+                flag = true;
+                for (int j=2; j<array[i]; j++){
+                    if (array[i] % j == 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) System.out.print(array[i]+" ");
+            }}
 //Вывод массива
+        System.out.println(" ");
+        System.out.print("massiv sort: ");
         for (int i=0; i< array.length;i++){
             System.out.print(array[i]+" ");
             k++;
