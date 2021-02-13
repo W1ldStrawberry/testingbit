59458d40b6f4323ef8147ca0a591f006cdbb8c02
W1ldStrawberry <maxonchik372@yandex.ru>
2021-02-14 01:21:01 +0300

    lab2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
 //Объявление и запись рандомного массива
-        int n = 50;
+        int n = 10;
         int min = 1000;
         int max = -1;
         int avg = 0;
         int k = 0;
+        int swap=0;
         int[] array = new int[n];
-        for (int i = 1; i< array.length;i++) {
-            array[i] = (int)(Math.random()*(200+1))-100;
+        for (int i = 0; i< array.length;i++) {
+            array[i] = (int)(Math.random()*(200+1)-100);
         }
 //поиск мин макс и среднего
         for (int i = 0; i< array.length;i++){
@@ -21,8 +22,14 @@ public class Main {
             avg += (int)(array[i] / array.length);
         }
         //сортировка пузырьком
-        //for (int i=1; i< array.length; i++){
-         //   for (j=1; j<i; i++)
+        for (int i=0; i< array.length-1; i++){
+            for (int j=0; j<array.length-i-1; j++){
+             if (array[j]>array[j+1]) {
+             swap = array[j];
+             array[j]=array[j+1];
+             array[j+1]=swap; }
+            }
+            }
       //  }
 //Вывод массива
         for (int i=0; i< array.length;i++){
